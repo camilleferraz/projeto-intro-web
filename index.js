397b0879ea22d2182e320b11c20ebf8ea5c411b1
camilleferraz <milleagf@gmail.com>
2022-10-27 17:52:31 -0300

    Projeto em fase-final

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 let planeta1 = {
 nome: "Mercúrio",
+imagem: "./img-projeto-web/mercury-background.png",
+link:"https://solarsystem.nasa.gov/planets/mercury/overview/",
 duracaoAno: 88,
 gravidade: 3.7, 
 atmosferaComposicao: ["Hélio","Sódio","Oxigênio"],
@@ -11,6 +13,8 @@ Curiosidade: ["É o planeta mais denso do sistema solar."]
 
 let planeta2 = {
 nome: "Vênus",
+imagem: "./img-projeto-web/netuno-background-new.png",
+link:"https://solarsystem.nasa.gov/planets/mercury/overview/",
 duracaoAno: 225,
 gravidade: 8.87, 
 atmosferaComposicao: ["CO2","N2"],
@@ -20,6 +24,8 @@ Curiosidade: ["Um dia em vênus é mais longo do que um ano."]
 
 let planeta3 = {
 nome: "Terra",
+imagem: "./img-projeto-web/earth-background.png",
+link:"https://solarsystem.nasa.gov/planets/mercury/overview/",
 duracaoAno: 365,
 gravidade: 9.8, 
 atmosferaComposicao: ["Nitrogênio","Oxigênio"],
@@ -40,6 +46,8 @@ arrayObjetosElementos.push(planeta1,planeta2,planeta3)
 
 const planeta4 = {
 nome: "Marte",
+imagem: "./img-projeto-web/mars-background.png",
+link:"https://solarsystem.nasa.gov/planets/mercury/overview/",
 duracaoAno: 687,
 gravidade: 3.721, 
 atmosferaComposicao: ["Nitrogênio","CO2"],
@@ -73,6 +81,8 @@ console.log(arrayObjetosElementos)
 
 const planeta6 = {
 nome: "Júpiter",
+imagem: "./img-projeto-web/jupiter-background.png",
+link:"https://solarsystem.nasa.gov/planets/mercury/overview/",
 duracaoAno: 4380,
 gravidade: 24.79, 
 atmosferaComposicao: ["Hidrogênio","CO2"],
@@ -137,4 +147,78 @@ function funcaoFiltro(array,string){
 
 funcaoFiltro(arrayObjetosElementos,"Marte")
 
+
+let listaPlanetas = document.querySelector(".card-wrapper")
+arrayObjetosElementos.map(planetas=>{
+    function listarPlanetas(){
+        listaPlanetas.innerHTML += ` <div class="card-conteudo swiper-slide" id="card">
+                       
+        <img src=${planetas.imagem} alt="" class="planeta">
+        <ul class="info">
+            <li><a href= target="_blank" class="titulo-planeta"><strong>${planetas.nome}</strong></a></li>
+            <li>Duração do ano: ${planetas.duracaoAno}</li>
+            <li>Gravidade: ${planetas.gravidade}</li>
+            <li>Composição Atmosférica:${planetas.atmosferaComposicao}</li>
+            <li>Pertence ao Sitema: ${planetas.sistemaSolar}</li>
+            <li>Curiosidade: ${planetas.Curiosidade}</li>
+        </ul>
+        
+    </div>`
+
+    }
+listarPlanetas()
+
+})
+
+function filtrarPesquisa(){
+
+    
+let valorDeBusca = document.querySelector(".input").value
+let planetaFiltrado = document.querySelector(".conteudo-filtrado")
+let campoBusca = document.querySelector(".input")
+
+planetaFiltrado.innerHTML=""
+campoBusca.innerHTML = ""
+
+if(valorDeBusca === ""){
+    alert("Ops.. Acho que você não digitou nada.")
+    valorDeBusca = ""
+    
+    
+}
+
+for(let planeta in arrayObjetosElementos){
+    // console.log(arrayObjetosElementos[planeta].nome)
+    console.log(campoBusca)
+    if(valorDeBusca === arrayObjetosElementos[planeta].nome){
+    planetaFiltrado.innerHTML += ` <div class="filter-card" id="card">
+                       
+        <img src=${arrayObjetosElementos[planeta].imagem} alt="" class="planeta">
+        <ul class="info">
+            <li><a href= target="_blank" class="titulo-planeta"><strong>${arrayObjetosElementos[planeta].nome}</strong></a></li>
+            <li><strong>Duração do ano:</strong> ${arrayObjetosElementos[planeta].duracaoAno}</li>
+            <li>Gravidade: ${arrayObjetosElementos[planeta].gravidade}</li>
+            <li>Composição Atmosférica:${arrayObjetosElementos[planeta].atmosferaComposicao}</li>
+            <li>Pertence ao Sitema: ${arrayObjetosElementos[planeta].sistemaSolar}</li>
+            <li>Curiosidade: ${arrayObjetosElementos[planeta].Curiosidade}</li>
+        </ul>
+        
+    </div>`
+    
+    listaPlanetas.innerHTML = ""
+    valorDeBusca.innerHTML = ""
+    
+    
+    
+}
+
+}
+
+}
+
+function conferirValue(){
+    let valueInput = document.querySelector(".input").value
+
+    console.log(valueInput)
+}
 
